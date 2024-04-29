5a4702e353fb077c833f34006fe68b999bc47c54
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2024-04-29 19:32:13 +0300

    test

---
+++ OPeratorDemo.js
@@ -0,0 +1,6 @@
+var a = 5
+console.log(typeof a)
+var b ="5"
+console.log(typeof b)
+console.log(a == b) // here we are checking whter the the value is same
+console.log(a === b) // here we are checking whether the value and the type of data is same
